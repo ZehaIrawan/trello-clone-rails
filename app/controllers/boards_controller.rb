dbc81e40546db0464c1982ccb1a428ad25013ed8
Zeha <zeha.irawan@yicom.com>
2023-12-24 06:00:29 +0800

    create board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,25 @@
 class BoardsController < ApplicationController
   def index
     @boards = current_user.boards
-    print(@boards,'abc' )
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+   @board = current_user.boards.new(board_params)
+
+    if @board.save
+      redirect_to @board, notice: 'Board was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name)
   end
 end
