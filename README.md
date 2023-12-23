bf0db351d94eded4157b81181ea3de4e3946c21e
Zeha <zeha.irawan@yicom.com>
2023-12-23 10:26:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+- Need to figure out logic for marketing site in domain.com and app in app.domain.com
+- config/initializers/devise.rb ignore or put the secret file in env
+
+Done
+- [x] Devise auth
+- [x] setup hot reload with rails_live_reload
+- [x] Removed const defaultTheme = require('tailwindcss/defaultTheme') because not found
+
+## Running the app
+Use bin/dev instead of rails s for tailwind to work
---
+++ config/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
